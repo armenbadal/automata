e00955b2020cb0c4114350de66e3d4f3c1d4573b
Արմեն Բադալյան <armenbadal@gmail.com>
2016-08-16 10:57:16 +0400

    Երկրորդ

---
+++ src/drive/Main.java
@@ -0,0 +1,17 @@
+package drive;
+
+import engine.Automata;
+
+/**/
+public class Main {
+    //
+    private void test_a()
+    {
+        Automata autom = new Automata();
+    }
+
+    public static void main( String[] args )
+    {
+        // TODO լրացնել
+    }
+}
